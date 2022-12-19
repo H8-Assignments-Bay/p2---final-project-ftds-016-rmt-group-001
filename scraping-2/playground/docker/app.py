12db1ffe9185f589259babc2a5e7f129d4bed7e4
Jason <jason.onggo@tempatkerja.com>
2022-12-19 15:53:11 +0700

    add proof of concept query parameter

--- scraping-2/playground/docker/app.py
+++ scraping-2/playground/docker/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, Response
+import json
 
 app = Flask(__name__)
 
 @app.route("/sentiment")
 def sentiment():
-    return "Hello World", 200
+    args = request.args
+    
+    symbol = args.get("symbol")
+    start_from = args.get("start_from")
+    end_at = args.get("end_at")
+    
+    return Response(
+        response=json.dumps(
+            {
+                "symbol": symbol,
+                "start_from": start_from,
+                "end_at": end_at
+            }
+        ), mimetype='application/json'
+    ), 200
